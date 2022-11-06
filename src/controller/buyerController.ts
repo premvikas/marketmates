560a6e49b5a8ec5a88d8eaf3d9bf03621a205597
premvikas <premvikas.v@gmail.com>
2022-11-06 09:43:20 +0530

    feature - adding crateOrder Api

--- src/controller/buyerController.ts
+++ src/controller/buyerController.ts
@@ -1,5 +1,6 @@
 import client from '../configs/database';
 import express from 'express';
+import format from 'pg-format';
 
 const getSellersList = async (req :express.Request,res: express.Response) => {
 
@@ -31,7 +32,57 @@ const getSellerCatalog = async (req :express.Request,res: express.Response) => {
     }
 }
 
+const createOrder = async (req :express.Request, res: express.Response) => {
+    try{
+        const {productData, buyerId} = req.body;
+        const {sellerId} = req.params;
+        ;
+        const productIds =  productData.map((eachData: any) => eachData.productId)
+   
+        const response = await client.query('SELECT product_name,product_price,id FROM product WHERE id = ANY($1::int[])',[productIds]);
+    
+        const productDetails = response.rows;
+
+        //const productIdsMappedToSeller = productDetails.map(eachData => eachData.product_id)
+       // let difference = productIds.filter(x => !arr2.includes(x));
+
+        let totalPrice = 0, totalQuantity = 0;
+
+        for(let i=0;i<productDetails.length;i++){
+            const eachProduct = productDetails[i];
+            const {product_price: price, id} = eachProduct;
+            totalPrice += price;
+            const {quantity} = productData.find(({productId} : any) => productId === Number(id))
+            totalQuantity += quantity;
+        }
+    
+        await client.query('BEGIN')
+        const insertOrderText = 'INSERT INTO order_v1 (buyer_id, seller_id, quantity, price, created_at) VALUES ($1,$2,$3,$4, current_timestamp) RETURNING *'
+        const insertOrderValue = [buyerId, sellerId, totalQuantity, totalPrice]
+    
+        const insertOrderResponse = await client.query(insertOrderText, insertOrderValue);
+        const orderId = insertOrderResponse.rows[0].id;
+       
+        const orderItems : any = [];
+        productDetails.forEach(((eachProduct : any) => {
+            const {id,  product_price} = eachProduct;
+            const {quantity} = productData.find(({productId}: any) => productId === Number(id))
+            orderItems.push([orderId, id, quantity, product_price, new Date()]);
+        }))
+    
+       await client.query(format('INSERT INTO order_item (order_id, product_id, quantity, unit_price, created_at) VALUES %L', orderItems));
+       await client.query('COMMIT');
+
+       return res.status(200).json({message: "success", orderId: orderId});
+    } catch (err){
+        console.log({err})
+        await client.query('ROLLBACK')
+        return res.status(400).json({error: err})
+    }
+}
+
 export {
     getSellersList,
-    getSellerCatalog
+    getSellerCatalog,
+    createOrder
 }
--- src/routes/buyerRouter.ts
+++ src/routes/buyerRouter.ts
@@ -3,10 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 import {authoriseUser } from '../controller/authController';
-import {getSellersList, getSellerCatalog} from '../controller/buyerController';
+import {getSellersList, getSellerCatalog, createOrder} from '../controller/buyerController';
 
 
 router.get('/getSellersList', authoriseUser, getSellersList)
 router.get('/getSellerCatalog/:sellerId', authoriseUser, getSellerCatalog)
+router.post('/createOrder/:sellerId',authoriseUser, createOrder)
 
 export = router;
